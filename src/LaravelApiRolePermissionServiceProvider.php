dcd4a56052350e329b99056bc5150fff5c019809
Sunil Kisoensingh <sunil.kisoensingh@gmail.com>
2020-02-09 21:40:15 +0100

    added service provider

---
+++ src/LaravelApiRolePermissionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WeSimplyCode\LaravelApiRolePermission;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelApiRolePermissionServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // code
+    }
+
+    public function register()
+    {
+        // code
+    }
+}
